e592179ad7d6e4d0baa7b55f019fbc25ef520b3e
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-13 15:40:00 +0000

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/**/
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

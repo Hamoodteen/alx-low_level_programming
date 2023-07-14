4198e5cbb698934df8ef7fc47d2ee1414ec99428
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-14 21:08:59 +0000

    0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

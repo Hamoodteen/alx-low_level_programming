e8d696bbd8e1477548e55b2d84100cf38bd8018d
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-14 19:21:38 +0000

    0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}   
+return(0);
+}

47f757e34ae6dd27408917c80b39ec3991394dd3
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-14 19:49:54 +0000

    0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("0123456789\n");
+}

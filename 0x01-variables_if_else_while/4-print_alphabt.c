1c03b3b96aa768de8bacf97c77a04b072d08e216
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-14 19:43:40 +0000

    0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'o' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'r' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+printf("\n");
+return (0);
+}

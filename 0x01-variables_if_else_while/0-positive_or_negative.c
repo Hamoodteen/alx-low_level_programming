62a39663c424a0cba59523d0af35fae4da202329
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-14 18:21:48 +0000

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* main */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;

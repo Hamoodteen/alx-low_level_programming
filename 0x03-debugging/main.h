8122df9ab969676e278af2c9cdff7e0e0456c872
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-18 19:38:02 +0000

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,23 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+};
+}
 
 #endif /* MAIN_H */

b442219ca7ee55ce924b772cfe444e61d7eaa5ca
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 02:25:53 +0000

    0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - 0
+ * @n: int
+ * Return
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+}
+else if (n == 0)
+{
+_putchar('0');
+}
+else if (n < 0)
+{
+_putchar('-');
+}
+}

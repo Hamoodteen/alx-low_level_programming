099bcf5428256b28eee4458e7c2b725165590d23
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 03:10:37 +0000

    0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,15 @@ int print_last_digit(int n)
 {
 if (n >= 0)
 {
-return (n % 10);
+n = (n % 10);
+_putchar(n + '0');
+return (n);
 }
 else
 {
-return (((-1) * n) % 10);
+n = (((-1) * n) % 10);
+_putchar(n + '0');
+return (n);
 }
 return (n);
 }

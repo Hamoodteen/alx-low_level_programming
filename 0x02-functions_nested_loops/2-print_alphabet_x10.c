5f4d9c165788351acd6d4c5351cd4080b31c167e
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 02:05:58 +0000

    0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 0
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int n;
+char i;
+for (n = 0 ; n <= 10 ; n++)
+{
+for (i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}

e489fd091c85eaa650ed14a1ed70451d6a913d43
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 19:29:11 +0000

    0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ for (i = n ; i <= 97 ; i++)
 {
 printf("%d, ", i);
 }
-printf(98);
+printf("98");
 }
 else if (n > 98)
 {
@@ -22,7 +22,7 @@ for (i = n ; i >= 99 ; i--)
 {
 printf("%d, ", i);
 }
-printf(98);
+printf("98");
 }
 else if (n == 98)
 {

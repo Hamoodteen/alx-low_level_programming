8abb6b0144af02ead15e6e83fad017ccbdd03c8a
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 15:22:11 +0000

    0

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * 24_hours- 0
+ * Return: void
+ */
+void jack_bauer(void)
+{
+for (int i = 00 ; i <= 23 ; i++)
+{
+for (int n = 00 ; i <= 59 ; i++)
+{
+_putchar(i);
+_putchar(':');
+_putchar(n);
+_putchar('\n');
+}
+}
+}

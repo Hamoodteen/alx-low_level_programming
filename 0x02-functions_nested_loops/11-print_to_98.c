6f56bca0943860ed01f8f4e974ad2eb5eec13740
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 19:28:27 +0000

    0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,21 +10,23 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
-for (i = n ; i <= 98 ; i++)
+for (i = n ; i <= 97 ; i++)
 {
-printf("%d, \n" , i);
+printf("%d, ", i);
 }
+printf(98);
 }
 else if (n > 98)
 {
-for (i = n ; i >= 98 ; i--)
+for (i = n ; i >= 99 ; i--)
 {
-printf("%d, " , i);
+printf("%d, ", i);
 }
+printf(98);
 }
 else if (n == 98)
 {
-printf("%d" , n);
+printf("%d", n);
 }
 printf("\n");
 }

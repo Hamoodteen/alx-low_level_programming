72064734030610044557c0dfc783475547af50e2
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-19 01:33:12 +0000

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,12 @@
 #include "main.h"
+/**
+ * print_alphabet - 0
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 char i;
-for(i = 'a' ; i <= 'z' ; i++)
+for (i = 'a' ; i <= 'z' ; i++)
 {
 _putchar(i);
 }

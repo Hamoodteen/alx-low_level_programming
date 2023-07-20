03d021f74d49cf393e9662a74e49d9ed48003e90
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 02:57:21 +0000

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
-* positive_or_negative - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
-if (i > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", n);
-}
-else if (i < 0)
-{
-printf("%d is negative\n", n);
-}
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }

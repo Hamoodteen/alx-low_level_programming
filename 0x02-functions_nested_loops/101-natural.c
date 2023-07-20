23d972b94fe6fbd90053614fb30c4f41230f00b6
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 02:09:12 +0000

    0

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - f
+ * return: 0
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0 ; i <= 1024 ; i++)
+{
+if (((i % 3) == 0) || ((i % 5) == 0))
+{
+j += i ;
+}
+}
+printf("%d\n", j);
+}

225486768a709e7d64b0fe1c8430ae221cc7e1e8
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 00:42:06 +0000

    0

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,27 @@
  */
 void jack_bauer(void)
 {
-int i;
-int n;
-for (i = 00 ; i <= 23 ; i++)
-{
-for (n = 00 ; n <= 59 ; n++)
-{
-if ((i < 10) || (n < 10))
-{
-printf("0%d:0%d\n", i, n);	
-}
-else
-{
-printf("%d:%d\n", i, n);
-}
-}
-}
+	int i;
+	int n;
+	for (i = 00; i <= 23; i++)
+	{
+		for (n = 00; n <= 59; n++)
+		{
+			if (i < 10)
+			{
+				if (n < 10)
+				{
+					printf("0%d:0%d\n", i, n);
+				}
+				else
+				{
+					printf("0%d:%d\n", i, n);
+				}
+			}
+			else
+			{
+				printf("%d:%d\n", i, n);
+			}
+		}
+	}
 }

b5461e6a1196afd35f4fe14d8909e961f489cbca
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 23:50:25 +0000

    0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - f
+ * Return: void
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,9 @@
 #define main_h
 
 #include <unistd.h>
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 #endif

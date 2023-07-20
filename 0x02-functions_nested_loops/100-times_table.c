ea0c9de242b5247287a386b5299409b6f99a23d4
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 01:37:55 +0000

    0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,7 +29,7 @@ printf("  %d", j);
 }
 else
 {
-printf(" %d,  ", j);
+printf(" %d, ", j);
 }
 }
 else
@@ -40,7 +40,7 @@ printf("%d", j);
 }
 else
 {
-printf("%d, ", j);
+printf("%d,  ", j);
 }
 }
 }

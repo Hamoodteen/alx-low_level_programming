0a51d8a4f36e1b3f97a6340262c7f889fdadc906
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-20 23:57:47 +0000

    0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - f
+ * Return: void
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+if ((i == 2) || (i == 4))
+{
+
+}
+else
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}

88b52a35a53483f11eac200eb91316a67a81fb24
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-21 01:13:30 +0000

    0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - f
+ * @size: int
+ * Return: void
+*/
+void print_square(int size)
+{
+int i;
+int j;
+for (i = 0 ; i < size ; i++)
+{
+for (j = 0 ; j < size ; j++)
+{
+if (size <= 0)
+{
+
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}

b8babb5c70fbfef99a99809f229f2c384acbfe81
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-21 01:35:13 +0000

    0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ if ((i % 3 == 0) && (i % 5 != 0))
 {
 printf("Fizz ");
 }
-else if ((i % 3 != 0) && (i % 5 == 0))
+else if ((i % 3 != 0) && (i % 5 == 0) && (i != 100))
 {
 printf("Buzz ");
 }
-else if ((i % 3 == 0) && (i % 5 ==0))
+else if ((i % 3 == 0) && (i % 5 ==0) && (i != 100))
 {
 printf("FizzBuzz ");
 }

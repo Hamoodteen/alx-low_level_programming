fd41cb502e50fa07511c22b9d4614e02b98729ba
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-21 00:09:01 +0000

    0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - f
+ * Return: void
+*/
+void more_numbers(void)
+{
+int i;
+int n;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (n = 0 ; n <= 14 ; i++)
+{
+if (n >= 10)
+{
+_putchar('1' + '0');
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+}

5ef86d5b4d606e832d10a031be082e07c9993320
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-26 00:29:59 +0000

    0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * strlen - f
+  * @c: char
+  * Return: integer
+*/
+int _strlen(char *s)
+{
+int i;
+int j = 0;
+for (i = 0 ; i <= sizeof(*s) ; i++)
+{
+j += i;
+j--;
+}
+_putchar(j + '0');
+}

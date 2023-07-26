622eb92dcd14bc96e5a137725c37ec7b4eb490b6
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-26 01:29:52 +0000

    0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,12 @@
 void puts2(char *str)
 {
 unsigned long int i;
-for (i = 0 ; str[i] != '\0' ; i += 2)
+int j = 0;
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0 ; i < j ; i += 2)
 {
 _putchar(str[i]);
 }

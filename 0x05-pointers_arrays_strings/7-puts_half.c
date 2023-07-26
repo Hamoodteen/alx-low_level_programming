f59321f6eb9440a76c361530996138d7c4031a9c
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-26 01:43:39 +0000

    0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * puts_half - f
+  * @str: char
+  * Return: int
+*/
+void puts_half(char *str)
+{
+unsigned long int i;
+unsigned long int j = 0;
+unsigned long int n;
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+j = i;
+}
+if (i != 0)
+{
+j++;
+}
+if (j % 2 == 0)
+{
+j = j / 2;
+}
+else if (j % 2 != 0)
+{
+j = ((j - 1) / 2);
+}
+for (n = j ; str[n] != '\0' ; i++)
+{
+_putchar(str[n]);
+}
+_putchar('\n');
+}

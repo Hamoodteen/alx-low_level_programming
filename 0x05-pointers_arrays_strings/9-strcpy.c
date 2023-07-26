da76e8642e930f1a5005dbbffc160fc2c0d7c2ea
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-26 02:37:40 +0000

    0

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _strcpy - f
+  * @dest: char
+  * @src: char
+  * Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+src = &dest;
+*src = dest;
+return (dest);
+}

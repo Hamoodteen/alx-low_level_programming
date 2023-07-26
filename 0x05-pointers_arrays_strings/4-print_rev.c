0e34d9c4ed0677154733dea112b1fb385a9030b4
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-26 01:05:22 +0000

    0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_rev - f
+  * @s: char
+  * Return: void
+*/
+void print_rev(char *s)
+{
+unsigned long int i;
+for (s[i] != '\0' ; i = 0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}

6259df29b4c6b244191416be8c796b63506690a5
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-29 21:29:04 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,12 @@
   */
 char *_strcat(char *dest, char *src)
 {
-	//
-	//
-	//
-	//
-	return (*src + *dest);
+int a, b;
+for (a = 0; dest[a] != '\0'; a++)
+;
+for (b = 0; src[b] != '\0'; b++)
+{
+dest[a + b] = src[b];
+}
+return (dest);
 }

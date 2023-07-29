008748e56b8a39d5d2c178400604c0b64dbf2051
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-29 21:23:46 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-  * _strcat - f
-  * @*dest: char
-  * @*src: char
+  * *_strcat - f
+  * @dest: char
+  * @src: char
   * Return: char
   */
 char *_strcat(char *dest, char *src)

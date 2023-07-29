fb1f09bc52b1de0d4f308ba4550bcc0a02d6994e
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-29 23:30:33 +0000

    0

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int a, b;
-if (sizeof(src) < n)
-{
+// if (sizeof(src) < n)
+// {
 for (a = 0; dest[a] != '\0'; a++)
 ;
 for (b = 0; (b < n && src[b] != '\0'); b++)
 {
 dest[a + b] = src[b];
 }
-}
+// }
 return (dest);
 }

6df5501456ce354bd380b5bb5421c8ad5354a1d7
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-29 21:58:57 +0000

    0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 int a, b;
 for (a = 0; dest[a] != '\0'; a++)
 ;
-for (b = 0; (src[b] < n && src[b] != '\0'); b++)
+for (b = 0; (b < n && src[b] != '\0'); b++)
 {
 dest[a + b] = src[b];
 }

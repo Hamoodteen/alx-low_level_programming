9386f62739cd04cb2991674f2f35749c3a4d5f5b
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-30 00:11:02 +0000

    0

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * string_toupper - Change all lowercase letters to uppercase
- * @c: string
- * Return: c
+ * string_toupper - f
+ * @a: char
+ * Return: char
  */
-char *string_toupper(char *c)
+char *string_toupper(char *a)
 {
-	int i;
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		if ((c[i] > 96) && (c[i] < 123))
-		{
-			c[i] -= 32;
-		}
-	}
-	return (c);
+int i;
+for (i = 0; a[i] != '\0'; i++)
+{
+if ((a[i] > 96) && (a[i] < 123))
+{
+a[i] -= 32;
+}
+}
+return (a);
 }

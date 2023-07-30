624af71bbdbc6177ab88772af3adeee34fe1e2e6
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-07-30 00:09:05 +0000

    0

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,13 @@
  */
 char *string_toupper(char *a)
 {
-int i;
-for (i = 0; a[i] != "\0"; i++)
-{
-if ((a[i] > 96) && (a[i] < 123))
-{
-a[i] -= 32;
-}
-}
-return (a);
+	int i;
+	for (i = 0; a[i] != "\0"; i++)
+	{
+		if ((a[i] > 96) && (a[i] < 123))
+		{
+			a[i] -= 32;
+		}
+	}
+	return (a);
 }

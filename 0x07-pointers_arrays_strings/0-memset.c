90e90f145eab391888486637d230662642b9f94e
Hamoodteen <ahmed104567@outlook.com>
2023-08-01 01:38:56 +0300

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - f
+ * @s: char
+ * @b: char
+ * @n: int
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0 ; i <= n ; i++)
+{
+s[i] = b;
+}
+return (s);
+}

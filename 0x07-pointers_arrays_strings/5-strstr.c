e0b03962342d2f389eaafb202e56a1d1c01eb2b7
Hamoodteen <ahmed104567@outlook.com>
2023-08-01 04:04:43 +0300

    0

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strpbrk - f
+ * @haystack: char
+ * @needle: char
+ * Return: s
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char s[3] = "hi!";
+haystack[5] = 'h';
+return (s);
+}

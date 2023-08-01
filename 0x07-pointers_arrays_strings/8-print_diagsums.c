10cddd44d9453104c75a4fc054157d379db651d1
Hamoodteen <ahmed104567@outlook.com>
2023-08-01 04:36:25 +0300

    0

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,12 +14,12 @@ int l;
 int b = 0;
 for (i = 0 ; i <= (size - 1) ; i++)
 {
-j += a[i][i];
+j += a[i];
 }
 printf("%d, ", j);
 for (l = size ; l >= 1 ; l--)
 {
-b += a[l][l];
+b += a[l];
 }
 printf("%d\n", b);
 }

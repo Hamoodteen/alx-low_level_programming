69975bbc8bd302dc49eaf2de1e07a37bd9bd70d4
Hamoodteen <ahmed104567@outlook.com>
2023-08-03 07:37:43 +0300

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - f
+ * @s: char
+ * Return: void
+*/
+void _puts_recursion(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++
+}
+_putchar('\n');
+}

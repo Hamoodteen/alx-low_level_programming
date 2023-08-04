dd7f7c23d16caec78a8654f42acbc6bf119a55a4
Hamoodteen <ahmed104567@outlook.com>
2023-08-04 11:33:10 +0300

    0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - f
+ * @n: int
+ * Return: i
+*/
+int is_prime_number(int n)
+{
+int i = 0;
+if ((n == 0) || (n == 1))
+{
+return (0);
+}
+if ((n % i) != ((n / 2) + 1))
+{
+is_prime_number(n + 1);
+}
+}

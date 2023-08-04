fbfc0590cea9fe19a74df7189c02ffe6ff1332ee
Hamoodteen <ahmed104567@outlook.com>
2023-08-04 10:15:46 +0300

    0

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,15 @@
 /**
  * _strlen_recursion - f
  * @s: char
- * Return: i
+ * Return: j
 */
 int _strlen_recursion(char *s)
 {
 int i = 0;
-if (*s != '\0')
+int j = 0;
+if (s[i] != '\0')
 {
-i = _strlen_recursion(s + 1);
+j = _strlen_recursion(s[i + 1]);
 }
-return (i);
+return (j);
 }

1cf522edc187436ab426865ce4637a019bea7d9f
Hamoodteen <ahmed104567@outlook.com>
2023-08-04 11:35:49 +0300

    0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,18 +2,18 @@
 /**
  * is_prime_number - f
  * @n: int
- * Return: i
+ * Return: int
 */
+int i = 0;
 int is_prime_number(int n)
 {
-int i = 0;
 if ((n == 0) || (n == 1))
 {
 return (0);
 }
 if ((n % i) != ((n / 2) + 1))
 {
-return (is_prime_number(n + 1));
+is_prime_number(n + 1);
 }
 return (1);
 }

257151df3a03379e6f8390f19720383f8ce7a896
Hamoodteen <ahmed104567@outlook.com>
2023-08-04 09:58:40 +0300

    0

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+if (*s != '\0')
 {
+_print_rev_recursion(s + 1);
 _putchar(*s);
-_print_rev_recursion(s - 1);
-}
-else
-{
-_putchar('\n');
-return;
 }
 }

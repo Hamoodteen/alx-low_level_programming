fc483753a413ebd5354f36438fe8b3cb0c131070
Hamoodteen <ahmed104567@outlook.com>
2023-08-04 11:55:24 +0300

    0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,5 @@
 /**
- * wildcmp - s
+ * wildcmp - f
  * @s1: char
  * @s2: char
  * Return: int
@@ -7,8 +7,12 @@
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
+{
 return (1);
+}
 if (*s1 == *s2)
+{
 return (wildcmp(s1 + 1, s2 + 1));
+}
 return (0);
 }

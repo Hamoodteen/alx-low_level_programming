ac50f8993633c3e8fdfd31c940ac0a5d7b48d6ea
Hamoodteen <ahmed104567@outlook.com>
2023-08-05 06:04:07 +0300

    0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,13 +11,16 @@ int main(int argc, char **argv)
 {
 int i;
 int z = 0;
-if ((isdigit(argv) != 0) && (argc != 1))
+if (argc != 1)
 {
 for (i = 1 ; i < argc ; i++)
+{
+if (isdigit(argv) != 0)
 {
 z += atoi(argv[i]);
 }
 printf("%d\n", z);
+}
 return (0);
 }
 else if (argc == 1)

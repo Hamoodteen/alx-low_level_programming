83e80bc970389ba260fa96d7a2f41560a16fd720
Hamoodteen <ahmed104567@outlook.com>
2023-08-05 05:25:31 +0300

    0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - f
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+(void)argv;
+int i;
+for (i = 1 ; i <= argc ; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}

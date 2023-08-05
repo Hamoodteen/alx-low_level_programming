b6db255ca5d38214617318528aa169aed990f555
Hamoodteen <ahmed104567@outlook.com>
2023-08-05 05:35:28 +0300

    0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,13 +7,16 @@
 */
 int main(int argc, char **argv)
 {
+int i = 1;
 if (argc == 2)
 {
-printf("%d\n", ((int)argv[1] * (int)argv[2]));
+i = ((int)argv[1] * (int)argv[2]);
+printf("%d\n", i);
 return (0);
 }
 else
 {
+printf("Error\n");
 return (1);
 }
 }

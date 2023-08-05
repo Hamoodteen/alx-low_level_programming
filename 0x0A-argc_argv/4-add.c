0f4e4426cad126dc8701172483d9068083908fdd
Hamoodteen <ahmed104567@outlook.com>
2023-08-05 05:49:03 +0300

    0

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - f
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+int i;
+int z = 0;
+if ((argc % 1 == 0) && (argc != 1))
+{
+for (i = 1 ; i < argc ; i++)
+{
+z += atoi(argv[i]);
+}
+printf("%d\n", z);
+return (0);
+}
+else if (argc == 1)
+{
+printf("%d\n", i);
+reeturn (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}

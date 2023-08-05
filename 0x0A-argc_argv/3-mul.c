ffcd994bbef16c1d98a590fc48019ffd02eb51ad
Hamoodteen <ahmed104567@outlook.com>
2023-08-05 05:31:24 +0300

    0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - f
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%d\n", (((int)argv[1]) * ((int)argv[2])));
+return (0);
+}

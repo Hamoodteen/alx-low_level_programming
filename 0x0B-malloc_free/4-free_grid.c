f10673ef223d82cb9c31c63fdb5378736e61b16b
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 08:03:56 +0300

    0

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,7 +8,11 @@
 void free_grid(int **grid, int height)
 {
 int i;
-	for(i = 0 ; i <= height ; i++)
+	if (grid == NULL)
+	{
+		return;
+	}
+	for (i = 0 ; i <= height ; i++)
 	{
 		free(grid[i]);
 	}

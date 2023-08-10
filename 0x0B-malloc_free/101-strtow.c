6c56aa5c3966c356c45eb22e67b7e380d43620c2
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 08:30:42 +0300

    0

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * strtow - f
+ * @str: char
+ * Return: char
+*/
+char **strtow(char *str)
+{
+int i;
+int j = 0;
+int k = 0;
+char **c;
+	if ((str == NULL) || (str == ""))
+	{
+		return (NULL);
+	}
+	for(i = 0 ; str[i] != '\0' ; i++)
+    {
+        if((str[i] == ' ') || (str[i] == '\0'))
+        {
+            c[k][j] = '\0';
+            k++;
+            j = 0;
+        }
+        else
+        {
+            c[k][j] = str[i];
+            j++;
+        }
+    }
+	return (c);
+}

080f5986acb64875751d7441d86e379bedd21397
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 09:42:10 +0300

    0

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,9 +16,9 @@ char *argstostr(int ac, char **av)
 		{
 			return (NULL);
 		}
-		for (i = 0; i < ac; b++)
+		for (i = 0 ; i < ac ; i++)
 		{
-			for (j = 0; av[i][j] != '\0'; c++)
+			for (j = 0 ; av[i][j] != '\0' ; c++)
 			{
 				cnt++;
 			}

07422d6187109fa58d1a9ad92472d0223f235002
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 04:10:10 +0300

    0

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * str_concat - f
+ * @s1: char
+ * @s2: char
+ * Return: s
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i;
+		s = (char *)malloc(sizeof(s1) + sizeof(s2) + 2);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			s[i] = s1[i];
+		}
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			s[i + sizeof(s1) + 1] = s2[i];
+		}
+		return (s);
+}

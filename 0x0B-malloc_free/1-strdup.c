70621fae47291759a2a4cc96e38becc13c7713a2
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 03:31:34 +0300

    0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strdup - f
+ * @str: char
+ * Return: s
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		s = malloc(sizeof(str));
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			s[i] = str[i];
+		}
+		return (s);
+}

8829746a5a56e62eafd3d9d2b88d4fbf9a13f055
Hamoodteen <ahmed104567@outlook.com>
2023-08-10 03:43:33 +0300

    0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,7 +7,7 @@
 char *_strdup(char *str)
 {
 	char *s;
-	unsigned long int i;
+	int i;
 		if (str == NULL)
 		{
 			return (NULL);
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		{
 			return (NULL);
 		}
-		for (i = 0; i <= sizeof(s); i++)
+		for (i = 0; str[i] != '\0'; i++)
 		{
 			s[i] = str[i];
 		}

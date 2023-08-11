a6dc85349d498a8a22cc7a6edeb91b8ef5e6e51b
Hamoodteen <ahmed104567@outlook.com>
2023-08-11 11:21:15 +0300

    0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p;
-	unsigned int a = (size * nmemb);
+	unsigned int a = (nmemb * size);
 		p = malloc(a);
 		memset(p, 0, a);
 		if ((nmemb == 0) || (size == 0) || (p == NULL))

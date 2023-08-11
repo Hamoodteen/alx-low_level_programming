8e89f9b2999a352402c9996198e353b284be364c
Hamoodteen <ahmed104567@outlook.com>
2023-08-11 10:29:41 +0300

    0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

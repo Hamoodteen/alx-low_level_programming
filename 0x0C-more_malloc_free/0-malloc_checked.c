4b737b5b022dc870853bb6e7d7c605165d695552
Hamoodteen <ahmed104567@outlook.com>
2023-08-11 10:22:25 +0300

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - f
+ * @b: int
+ * Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+		p = malloc(b);
+		if (p == NULL)
+		{
+			exit(98);
+		}
+}

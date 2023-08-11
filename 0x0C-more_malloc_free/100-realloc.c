144039f316733c6383d8e9364b60cee63b7d6a77
Hamoodteen <ahmed104567@outlook.com>
2023-08-11 12:26:05 +0300

    0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			return (ptr);
 		}
-		if ((new_size == 0) && (ptr != NULL))
+		if ((new_size == (unsigned int)0) && (ptr != NULL))
 		{
 			free(ptr);
 			return (NULL);
@@ -22,19 +22,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (ptr == NULL)
 		{
 			free(new_ptr);
-			new_ptr = malloc(new_size);
+			new_ptr = (int *)malloc(new_size);
 		}
 		if (new_size > old_size)
 		{
 			free(new_ptr);
-			new_ptr = malloc(new_size);
-			memcpy(new_ptr, ptr, new_size);
+			new_ptr = (int *)malloc(new_size);
+			(int *)memcpy(new_ptr, ptr, new_size);
 		}
 		if (new_size < old_size)
 		{
 			free(new_ptr);
-			new_ptr = malloc(old_size);
-			memcpy(new_ptr, ptr, old_size);
+			new_ptr = (int *)malloc(old_size);
+			(int *)memcpy(new_ptr, ptr, old_size);
 		}
 		if (new_ptr == NULL)
 		{

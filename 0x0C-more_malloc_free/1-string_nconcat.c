de3bbbbd463c056cceed4d6c1e63a97a63f6dc4e
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-08-12 21:22:31 +0300

    0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			b += n;
 		}
-		s = (char *)malloc((a * sizeof(*s1)) + (b * sizeof(*s2)) + 1);
+		s = (char *)malloc((a * sizeof(*s1)) + (b * sizeof(*s2)) - 1);
 		if (s == NULL)
 		{
 			return (NULL);

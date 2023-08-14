250440affa32e6393c567ac919ca0984478975a9
Hamoodteen <ahmed104567@outlook.com>
2023-08-14 19:22:40 +0300

    0

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 #define ABS(x) ((x >= 0) ? (x) : ((x) * -1))
 #endif /* ABS(x) */

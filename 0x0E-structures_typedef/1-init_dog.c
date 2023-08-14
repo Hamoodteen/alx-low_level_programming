ad818719e6e30c4463b9b524866044228a971569
Hamoodteen <ahmed104567@outlook.com>
2023-08-14 15:45:39 +0300

    0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - f
+ * @d: struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog1;
+	d = &dog1;
+	d->name;
+	d->age;
+	d>owner;
+}

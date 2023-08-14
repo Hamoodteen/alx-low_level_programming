cfcde24ed5981f7fef97de0806c7c2816874c03a
Hamoodteen <ahmed104567@outlook.com>
2023-08-14 17:48:48 +0300

    0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog;
+	dog = malloc(sizeof(name) + sizeof(age) + sizeof(owner));
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;

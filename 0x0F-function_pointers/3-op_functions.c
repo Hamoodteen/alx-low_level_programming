d1cf2f58c2c131123d7cb37bb4d53ea760f46b1f
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 16:45:53 +0300

    0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - f
+ * @a: int
+ * @b: int
+ * Return: a + b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - f
+ * @a: int
+ * @b: int
+ * Return: a - b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - f
+ * @a: int
+ * @b: int
+ * Return: a * b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - f
+ * @a: int
+ * @b: int
+ * Return: a / b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - f
+ * @a: int
+ * @b: int
+ * Return: a % b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

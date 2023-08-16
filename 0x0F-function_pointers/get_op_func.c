9a1627a02aeb9be0611a709730a384bbd72274a2
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 16:49:11 +0300

    0

--- 0x0F-function_pointers/get_op_func.c
+++ 0x0F-function_pointers/get_op_func.c
@@ -1,10 +1,19 @@
 #include "3-calc.h"
-op_t ops[] = {
-    {"+", op_add},
-    {"-", op_sub},
-    {"*", op_mul},
-    {"/", op_div},
-    {"%", op_mod},
-    {NULL, NULL}
-};
-int i;
+/**
+ * get_op_func - f
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int (*get_op_func(char *s))(int a, int b)
+{
+	 op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+}

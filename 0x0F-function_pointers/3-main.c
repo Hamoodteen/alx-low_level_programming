646f78b8e13f3997a6a3a2bd2226be2c50873fb7
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 22:34:20 +0300

    0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,6 +7,27 @@
 */
 int main(int argc, char **argv)
 {
-	int num1, num2;
-	return (0);
+	int num1 = 0;
+	int num2 = 0;
+	char *operator;
+		if (argc > 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		num1 = atoi(argv[1]);
+		operator = argv[2];
+		if ((operator != "+") || (operator != "-") || (operator != "*") || (operator != "/") || (operator != "%"))
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		num2 = atoi(argv[3]);
+		if (((operator == "/") || (operator == "%")) && (num2 == 0))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", get_op_func(operator)(num1, num2));
+		return (0);
 }

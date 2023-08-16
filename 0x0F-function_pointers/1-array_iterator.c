fd5811b02df3064f43e4f51f13bedfa7438f55c0
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 15:05:11 +0300

    0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,4 @@
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 
 #include <unistd.h>
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

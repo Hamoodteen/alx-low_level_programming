cdc7236fdc107656fb48a88379936c59390d7aba
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 15:53:14 +0300

    0

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+/**
+ * int_index - f
+ * @array: int
+ * @size: int
+ * @cmp: func
+ * Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int j = 0;
+		if ((array == NULL) || ((cmp) == NULL))
+		{
+			return (NULL);
+		}
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0 ; i < size ; i++)
+		{
+			if (array[i] == cmp)
+			{
+				j = i;
+				break;
+			}
+		}
+		if (j == 0)
+		{
+			return (-1);
+		}
+		return (j);
+}

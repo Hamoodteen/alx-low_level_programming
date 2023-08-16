b9d5dc4658e554b5969b8bb7ddd7ff4a75272d75
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 15:23:07 +0300

    0

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+		if ((array == NULL) || (size == NULL))
+		{
+			return;
+		}
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }

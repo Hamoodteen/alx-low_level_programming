a85809c90f125381be9106a2c1a60d3210573ac5
Hamoodteen <ahmed104567@outlook.com>
2023-08-16 16:01:30 +0300

    0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	int j = 0;
-		if ((array == NULL) || ((cmp) == NULL))
-		{
-			return (0);
-		}
-		if (size <= 0)
+		if ((array == NULL) || ((cmp) == NULL) || (size <= 0))
 		{
 			return (-1);
 		}

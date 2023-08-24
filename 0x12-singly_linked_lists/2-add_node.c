0af006156f9b8553596de7ecdfd6e46657f89ba4
Hamoodteen <ahmed104567@outlook.com>
2023-08-25 01:25:44 +0300

    0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,20 @@
 #include <string.h>
 #include "lists.h"
 /**
+  * _strlen - f
+  * @s: char
+  * Return: i
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+/**
   * add_node - f
   * @head: list_t
   * @str: char
@@ -16,7 +30,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL || head == NULL)
 		return (NULL);
-	l = strlen(str);
+	l = _strlen(str);
 	copy = malloc((l + 1) * sizeof(char));
 	if (copy == NULL)
 	{

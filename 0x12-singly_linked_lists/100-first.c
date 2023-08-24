9bf82a6896dc05c044a4d0f414b46cf0504923a3
Hamoodteen <ahmed104567@outlook.com>
2023-08-25 02:26:02 +0300

    0

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,10 @@
  * print_string_before_main - f
  * Return: void
 */
-void print_string_before_main(void) {
-  printf("This string is printed before main\n");
+void print_string_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
 __attribute__((constructor)) void before_main(void)
 {
 	print_string_before_main();

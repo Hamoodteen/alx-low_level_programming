ce1bf62ece6d4dced2e10e592347b5920ccf2429
Hamoodteen <ahmed104567@outlook.com>
2023-08-25 02:08:13 +0300

    0

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,15 +6,7 @@
   */
 void free_list(list_t *head)
 {
-	list_t *h;
-	char *s;
-	size_t l;
-
-	h = head;
-	s = head->str;
-	l = head->len;
-	h->str = s;
-	h->len = l;
-	h->next = head;
-	free(h);
+	free(head->str);
+	free(head->next);
+	free(head);
 }

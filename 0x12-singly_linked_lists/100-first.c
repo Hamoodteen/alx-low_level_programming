ea7d3f421cedf8b1f6b59c697bdb1d481477ff4a
Hamoodteen <ahmed104567@outlook.com>
2023-08-25 02:26:59 +0300

    0

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,8 @@
 */
 void print_string_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 __attribute__((constructor)) void before_main(void)
 {

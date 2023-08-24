8eba2f8c9b16c6b2857af3433e2f894ded83926f
Hamoodteen <ahmed104567@outlook.com>
2023-08-25 00:50:40 +0300

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++)
 	{
 		if (h->str == NULL)
-			printf("[%d] [0] (nil)\n", h->len);
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;

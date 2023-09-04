269bbc30431c3e3d86e02016a6cc9d47cc773671
Hamoodteen <ahmed104567@outlook.com>
2023-09-04 19:20:01 +0300

    0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,5 @@
 #include "main.h"
-void tostring(char [], unsigned long int);
-/**
- * tostring - f
- * @str: char
- * @num: int
- * Return: void
-*/
-void tostring(char str[], unsigned long int num)
-{
-	unsigned long int i;
-	unsigned long int rem;
-	unsigned long int len;
-	unsigned long int n;
-
-	n = num;
-	len = 0;
-	while (n != 0)
-	{
-		len++;
-		n /= 10;
-	}
-	for (i = 0; i < len; i++)
-	{
-		rem = num % 10;
-		num = num / 10;
-		str[len - (i + 1)] = rem + '0';
-	}
-	str[len] = '\0';
-}
+#include <stdio.h>
 /**
  * print_binary - f
  * @n: int
@@ -38,10 +10,7 @@ void print_binary(unsigned long int n)
 	unsigned long int bin = 0;
 	unsigned long int f = 1;
 	unsigned long int rem;
-	int i;
-	char *c;
 
-	c = '\0';
 	while (n != 0)
 	{
 		rem = ~(n & 2);
@@ -49,9 +18,5 @@ void print_binary(unsigned long int n)
 		f *= 10;
 		n = n >> 2;
 	}
-	tostring(c, bin);
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		_putchar(i);
-	}
+	printf("%lu", bin);
 }

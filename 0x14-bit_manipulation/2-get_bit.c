3a0409df43cf32bdc224507a3e06ac8b05b67423
Hamoodteen <ahmed104567@outlook.com>
2023-09-05 19:25:25 +0300

    0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -65,7 +65,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
-	if (nobd(binary(n)) < index)
+	if (nobd(n) < index)
 		return (0);
 	rev = revbinary(binary(n));
 	rev >>= index;

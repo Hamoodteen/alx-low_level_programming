5fa41fbc85a295b39f02a35a5f14002f020d091b
Hamoodteen <ahmed104567@outlook.com>
2023-09-06 23:48:57 +0300

    0

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * read_textfile - f
+ * @filename: char
+ * @letters: int
+ * Return: 0 or 1
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fd;
+	char *g;
+	char *c;
+	int p;
+
+	c = NULL;
+	if (filename == NULL)
+		return (0);
+	fd = fopen(filename, O_RDONLY);
+	if (fd == NULL)
+		return (0);
+	g = fgets(c, letters, fd);
+	if (g == NULL)
+		return (0);
+	while (g != NULL)
+		p = printf("%s", c);
+	fclose(fd);
+	return (p);
+}

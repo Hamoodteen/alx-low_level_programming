1a8fb4b3093c74b07fd7d5a9e87f3a19f1dee6c9
Hamoodteen <ahmed104567@outlook.com>
2023-09-07 00:16:04 +0300

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,17 +9,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fd;
 	char *c;
+	char *f;
 	int p;
 
 	c = NULL;
 	if (filename == NULL)
 		return (0);
-	fd = fopen(filename, O_RDONLY);
+	fd = fopen(filename, "r");
 	if (fd == NULL)
 		return (0);
-	p = printf("%s", fgets(c, letters, fd));
-	if ((p < 0) || (letters > strlen(c)))
+	c = malloc(letters + 1);
+	if (c == NULL)
+	{
+		fclose(fd);
 		return (0);
+	}
+	f = fgets(c, (letters + 1), fd);
+	if (f == NULL)
+	{
+		free(c);
+		fclose(fd);
+		return (0);
+	}
+	if (letters < strlen(c))
+		p = fprintf(stdout, "%s", c);
+	else
+		p = fprintf(stderr, "%s", c);
+	if (p < 0)
+		return (0);
+	free(c);
 	fclose(fd);
 	return (p);
 }

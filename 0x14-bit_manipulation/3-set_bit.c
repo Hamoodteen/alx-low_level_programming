f0428f255c0ca0c27c8dcb762ee5d25cc568eb54
Hamoodteen <ahmed104567@outlook.com>
2023-09-06 11:33:04 +0300

    0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -51,7 +51,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	num = *n;
 	temp = malloc(sizeof(char) * 64);
 	temp = dectobin(num);
-	if (index > 63)
+	if (index > (sizeof(temp) - 1))
 		return (-1);
 	temp[index] = '1';
 	num = bintodec(temp);

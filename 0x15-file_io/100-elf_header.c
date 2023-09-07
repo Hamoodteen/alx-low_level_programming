0cb38959c0654ca6b93a81d1ed06ff77f52b1756
Hamoodteen <ahmed104567@outlook.com>
2023-09-07 03:25:43 +0300

    0

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <elf.h>
+/**
+ * main - f
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int __attribute__((__unused__)) argc,
+char __attribute__((__unused__)) **argv)
+{
+	return (0);
+}

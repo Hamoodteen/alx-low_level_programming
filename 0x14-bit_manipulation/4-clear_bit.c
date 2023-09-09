a7ac5ee6a29f2133937ec9154566dcad5bf3c96b
Hamoodteen <ahmed104567@outlook.com>
2023-09-10 00:21:41 +0300

    0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,59 +1,23 @@
 #include "main.h"
-/**
- * dectobin - f
- * @dec: int
- * Return: bin
-*/
-char *dectobin(int dec)
-{
-	char *bin;
-	int i = 0;
-
-	bin = malloc(sizeof(char) * 64);
-	while (dec != 0)
-	{
-		bin[i++] = (dec % 2) ? '1' : '0';
-		dec /= 2;
-	}
-	bin[i] = '\0';
-	return (bin);
-}
-/**
- * bintodec - f
- * @bin: char
- * Return: dec
-*/
-int bintodec(char *bin)
-{
-	int dec = 0;
-	int pow = 1;
-	int i;
-
-	for (i = (strlen(bin) - 1) ; i >= 0 ; i--)
-	{
-		if (bin[i] == '1')
-			dec += pow;
-		pow *= 2;
-	}
-	return (dec);
-}
+#include <stdio.h>
 /**
  * clear_bit - f
- * @n: ptr
+ * @n: int
  * @index: int
- * Return: 1 or -1
+ * Return: int
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num;
-	char *temp;
+	unsigned long int powerOfZero;
+	unsigned int bit;
 
-	num = *n;
-	temp = malloc(sizeof(char) * 64);
-	temp = dectobin(num);
-	if (index > strlen(temp))
+	if (index > 64)
 		return (-1);
-	temp[index] = '0';
-	num = bintodec(temp);
+	bit = index;
+	for (powerOfZero = 1; bit > 0; powerOfZero *= 2, bit--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= powerOfZero;
 	return (1);
 }

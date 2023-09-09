8fa15a198dda1bdf620a987711e6fb45ac113c5a
Hamoodteen <ahmed104567@outlook.com>
2023-09-10 00:16:52 +0300

    0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,59 +1,15 @@
 #include "main.h"
-/**
- * dectobin - f
- * @dec: int
- * Return: bin
-*/
-char *dectobin(int dec)
-{
-	char *bin;
-	int i = 0;
 
-	bin = malloc(sizeof(char) * 64);
-	while (dec != 0)
-	{
-		bin[i++] = (dec % 2) ? '1' : '0';
-		dec /= 2;
-	}
-	bin[i] = '\0';
-	return (bin);
-}
-/**
- * bintodec - f
- * @bin: char
- * Return: dec
-*/
-int bintodec(char *bin)
-{
-	int dec = 0;
-	int pow = 1;
-	int i;
-
-	for (i = (strlen(bin) - 1) ; i >= 0 ; i--)
-	{
-		if (bin[i] == '1')
-			dec += pow;
-		pow *= 2;
-	}
-	return (dec);
-}
-/**
- * set_bit - f
- * @n: ptr
- * @index: int
- * Return: 1 or -1
-*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num;
-	char *temp;
+  unsigned long int powerOfOne;
 
-	num = *n;
-	temp = malloc(sizeof(char) * 64);
-	temp = dectobin(num);
-	if (index > strlen(temp))
-		return (-1);
-	temp[index] = '1';
-	num = bintodec(temp);
-	return (1);
+  if (index > 64)
+    return (-1);
+
+  for (powerOfOne = 1; index > 0; index--, powerOfOne *= 2)
+    ;
+  *n += powerOfOne;
+
+  return (1);
 }

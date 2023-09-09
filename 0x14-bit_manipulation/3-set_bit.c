5dac8727b5dceaf5447ce3f146478ce1d8b1e280
Hamoodteen <ahmed104567@outlook.com>
2023-09-10 00:19:01 +0300

    0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
+/**
+ * set_bit - f
+ * @n: int
+ * @index: index
+ * Return: int
+*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
-  unsigned long int powerOfOne;
+	unsigned long int powerOfOne;
 
-  if (index > 64)
-    return (-1);
+	if (index > 64)
+		return (-1);
 
-  for (powerOfOne = 1; index > 0; index--, powerOfOne *= 2)
-    ;
-  *n += powerOfOne;
-
-  return (1);
+	for (powerOfOne = 1; index > 0; index--, powerOfOne *= 2)
+		;
+	*n += powerOfOne;
+	return (1);
 }

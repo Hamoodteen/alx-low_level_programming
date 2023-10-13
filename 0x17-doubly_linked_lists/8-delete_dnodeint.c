8f34b03aac19871353ebde0bd56e32da70e08cb3
Hamoodteen <ahmed104567@outlook.com>
2023-10-14 02:42:07 +0300

    0

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - f
+ * @head: head
+ * @index: index
+ * Return: int
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	(void)head;
+	(void)index;
+	return (1);
+}

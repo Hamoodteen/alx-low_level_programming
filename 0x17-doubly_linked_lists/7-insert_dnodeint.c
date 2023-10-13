975459e65a469b967d67348681cff2d4a91e5017
Hamoodteen <ahmed104567@outlook.com>
2023-10-14 02:35:46 +0300

    0

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - f
+ * @h: head
+ * @idx: int
+ * @n: int
+ * Return: 0
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	(void)h;
+	(void)idx;
+	(void)n;
+	return (0);
+}

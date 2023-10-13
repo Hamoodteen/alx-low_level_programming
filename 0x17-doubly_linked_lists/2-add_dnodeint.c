3f8f962bac10ea918e0abf54709ecb2f47c2b3a9
Hamoodteen <ahmed104567@outlook.com>
2023-10-14 01:56:12 +0300

    0

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * add_dnodeint - f
+  * @head: list_t
+  * @n: int
+  * Return: address
+  */
+dlistint_t *add_nodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nn;
+	int copy;
+
+	if (head == NULL)
+		return (NULL);
+	memcpy(&copy, &n, sizeof(int));
+	nn = malloc(sizeof(dlistint_t));
+	if (nn == NULL)
+	{
+		free(nn);
+		return (NULL);
+	}
+	nn->n = copy;
+	nn->next = *head;
+	*head = nn;
+	return (nn);
+}

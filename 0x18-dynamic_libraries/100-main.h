0eb55ce570c63134d7828c13a4dc00f949f3e87b
Hamoodteen <ahmed104567@outlook.com>
2023-10-17 01:50:34 +0300

    0

---
+++ 0x18-dynamic_libraries/100-main.h
@@ -0,0 +1,10 @@
+#ifndef _100_main_H_
+#define _100_main_H_
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/100-sums.c
@@ -0,0 +1,55 @@
+#include "100-main.h"
+/**
+ * add - 0
+ * @a: int
+ * @b: int
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - 0
+ * @a: int
+ * @b: int
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - 0
+ * @a: int
+ * @b: int
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - 0
+ * @a: int
+ * @b: int
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - 0
+ * @a: int
+ * @b: int
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}

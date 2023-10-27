eb9af0b31e89723b99ab761350fefd084a6fadad
Hamoodteen <ahmed104567@outlook.com>
2023-10-27 18:04:38 +0200

    0

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /$PWD/libtest.so https://github.com/Hamoodteen/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/$PWD/libtest.so
+wget -O /root/alx-low_level_programming/0x18-dynamic_libraries/libtest.so https://github.com/Hamoodteen/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/root/alx-low_level_programming/0x18-dynamic_libraries/libtest.so

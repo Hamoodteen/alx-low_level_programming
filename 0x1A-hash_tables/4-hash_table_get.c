7a2ed10eac45b22eb3e05edc346e10cd49b0fd77
Hamoodteen <ahmed104567@outlook.com>
2023-11-03 03:48:43 +0200

    0

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - f
+ * @ht: ht
+ * @key: key
+ * Return: char
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	(void)ht;
+	(void)key;
+	return (NULL);
+}

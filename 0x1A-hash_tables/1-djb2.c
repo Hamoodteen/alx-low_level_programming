b887d253c96cc5418789b3b5c73d3ca494ed3807
Hamoodteen <ahmed104567@outlook.com>
2023-11-03 03:23:40 +0200

    0

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,11 @@
+#include "hash_tables.h"
+
 /**
  * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
+ * @s: string used to generate hash value
  * Return: hash value
  */
-unsigned long int hash_djb2(const unsigned char *s)
+size_t hash_djb2(const unsigned char *s)
 {
 	unsigned long int hs;
 	int i;

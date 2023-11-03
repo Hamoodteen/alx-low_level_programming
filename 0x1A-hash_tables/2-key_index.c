a651f1148494fcbe49d79a1f2733cec3de95d19d
Hamoodteen <ahmed104567@outlook.com>
2023-11-03 03:46:07 +0200

    0

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - f
+ * @key: key
+ * @size: size
+ * Return: int
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	(void)key;
+	(void)size;
+	return (0);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - f
+ * @ht: ht
+ * @key: key
+ * @value: value
+ * Return: int
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	(void)ht;
+	(void)key;
+	(void)value;
+	return (0);
+}

402662be47134582e9fe57f97b49cff151834b5b
Hamoodteen <ahmed104567@outlook.com>
2024-02-05 22:30:10 +0200

    0

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - f
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: int
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+	(void)array;
+	(void)size;
+	(void)value;
+	return (0);
+}
---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - f
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: int
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	(void)array;
+	(void)size;
+	(void)value;
+	return (0);
+}
